57ad0da316dbb393e848a2f0705d66c1597074a8
admin <ajaniarafath@gmail.com>
2025-02-25 23:15:44 +0530

    first commit

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
         <title>My Website</title>
         <meta name="description" content="A cool Next.js website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
       </>
     );
   }
